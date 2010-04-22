c8bc66a4b47fb6c464bf85a6899e5bce3bec30cc
Brendan Hickey <brendan@bhickey.net>
2010-04-22 13:16:37 -0400

    Added function stubs to qual.

--- qual.h
+++ qual.h
@@ -6,8 +6,13 @@
 // Log-likeihood error probabilities from phred scores
 double PHRED_TABLE[] = {0.5};
 
-//default: assume that we're using Sanger reads
+// TODO Replace these stubs
+#define QUAL2LL(X) (1.0)
+#define QUAL2ERROR(X) (1.0)
+#define LOGERROR(X) (0.0)
 
+
+//default: assume that we're using Sanger reads
 #define LOGQUALITY(X) (PHRED_TABLE[X-33])
 #define LOGQUALITY_SANGER(X) (PHRED_TABLE[X-33])
 #define LOGQUALITY_ILLUMINA(X) (PHRED_TABLE[X-64])
