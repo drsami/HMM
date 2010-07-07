d3b6e860135b60a02a5f6ca3a9c760b0b2d0ff89
Brendan Hickey <brendan@bhickey.net>
2010-07-07 13:38:19 -0400

    Hacked up the hasher to support more hash extraction functions under a fixed encoding scheme.

--- hasher.cpp
+++ hasher.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 Hash::Hash(dynamic_bitset<> mask, dynamic_bitset<> val){
   _id = HID++;
-  _value = val;
+  _value = val & mask;
   _mask = mask;
   _size = mask.size();
-  assert(_mask.size() == _value.size());
 }
 
 bool Hash::operator<(const Hash rhs) const {
-  return _value < rhs._value;
+  return (_value & _mask) < (rhs._value & rhs._mask);
 }
 
 bool Hash::operator>(const Hash rhs) const {
-  return _value > rhs._value;
+  return (_value & _mask) > (rhs._value & rhs._mask);
 }
 
 bool Hash::match(dynamic_bitset<> t){
@@ -32,7 +31,9 @@ bool Hash::match(dynamic_bitset<> val, dynamic_bitset<> valmask){
 }
 
 // HashSet
+HashSet::HashSet(){
 
+}
 
 bool HashSet::insert(Hash h){
 
@@ -105,22 +106,163 @@ Sequence::Sequence(kseq_t* kseq){
   if((len = kseq->qual.l)){
     _qual = string(kseq->qual.s, len); 
   } else {
-    _qual = ""; 
+    _qual = "";   
   };
 
+  _gapMask = dynamic_bitset<>(len*2);
+
   // construct the mask
-/*  string::iterator s_itr;
+  string::iterator s_itr;
   for( s_itr = _seq.begin(); s_itr != _seq.end(); s_itr++ ){
+    _gapMask <<= 2;
     if( isNucleotide(*s_itr) ){
-      _mask.push_back( true );  _mask.push_back( true );
-    } else {
-      _mask.push_back( false ); _mask.push_back( false );
+      _gapMask |= dynamic_bitset<>(2,3);
     }
   }
-  */
 }
 
+// MultipleSequenceAlign
 
+void MultipleSequenceAlign::reset(){
+    _consensus.clear();
+    _gaps.clear();
+    _mask.clear();
+}
+
+// Insert a sequence into the multiple sequence alignment
+int MultipleSequenceAlign::insert(kseq_t *kseq){
+  Sequence seq(kseq);
+  _sequences.push_back(seq);
+  reset();
+  return _sequences.size();
+}
+
+bool MultipleSequenceAlign::remove(string seq){
+  vector<Sequence>::iterator s_itr;
+  for(s_itr = _sequences.begin(); s_itr != _sequences.end(); s_itr++){
+    if( s_itr->getName() == seq ){
+      _sequences.erase(s_itr);
+      reset();
+      return true;
+    }
+  }
+  return false;
+}
+
+dynamic_bitset<> MultipleSequenceAlign::getGaps(){
+  if( _gaps.size() > 0){
+    return _gaps;
+  }
+
+  dynamic_bitset<> mask(0);
+  if( _sequences.size() ){
+    mask = _sequences[0].getMask();
+
+    vector<Sequence>::iterator s_itr = _sequences.begin() + 1;
+    for( ; s_itr != _sequences.end(); s_itr++ ){
+      mask &= s_itr->getMask();
+    }
+  }
+
+  _gaps = mask;
+  return mask;
+}
+
+dynamic_bitset<> MultipleSequenceAlign::getConsensus(){
+    if( _consensus.size() > 0 ){
+        return _consensus;
+    }
+
+    dynamic_bitset<> consensus(0);
+  if( _sequences.size() ){
+    consensus = _sequences[0].getEncoded();
+
+    vector<Sequence>::iterator s_itr = _sequences.begin() + 1;
+    for( ; s_itr != _sequences.end(); s_itr++ ){
+      consensus &= s_itr->getEncoded();
+    }
+  }
+
+  _consensus = consensus;
+  return consensus;
+}
+
+dynamic_bitset<> MultipleSequenceAlign::getMask(){
+    if( _mask.size() > 0 ){
+        return _mask;
+    }
+
+    dynamic_bitset<> prev = _sequences[0].getEncoded();
+    dynamic_bitset<> accumulator = dynamic_bitset<>(prev.size());
+    accumulator.flip();
+
+
+    vector<Sequence>::iterator s_itr = _sequences.begin() + 1;
+    for( ; s_itr != _sequences.end(); s_itr++ ){
+        dynamic_bitset<> curr = s_itr->getEncoded();
+        accumulator -= (curr ^ prev);
+        prev = curr;
+    }
+
+    dynamic_bitset<> gaps = getGaps();
+    accumulator &= gaps;
+    _mask = accumulator;
+    return accumulator;
+}
+
+HashSet MultipleSequenceAlign::makeHashes(){
+  
+  HashSet hs;
+  multiset<Hash> h;
+  
+  int hash_length = 24;
+
+  dynamic_bitset<> consensus = getConsensus();
+  dynamic_bitset<> gaps      = getGaps();
+  dynamic_bitset<> mask      = getMask();
+
+  int len = mask.size();
+
+  dynamic_bitset<> hashMask(hash_length);     // tell us /where/ the invariant bits are in the hash
+  dynamic_bitset<> hashSequence(hash_length); // tell us /what/ the invariant bits are in the hash
+
+  dynamic_bitset<> gapWindow(hash_length); // Is there a gap here?
+  // buffer
+  for( int ii = 0; ii < hash_length; ii++ ){
+    hashMask[ii]     = mask[ii];
+    hashSequence[ii] = consensus[ii];
+    gapWindow[ii]    = gaps[ii];
+  }
+
+  for( int ii = hash_length; ii < len; ii++ ){
+      // The least significant bit is unmasked and we aren't in a gap
+      if( hashMask[0] && (~gapWindow).none() ){
+         Hash hash(hashMask, hashSequence);
+         h.insert(hash);
+      }
+
+      // right shift everything dumping the least significant bit
+      hashMask     >>= 1;
+      hashSequence >>= 1;
+      gapWindow    >>= 1;
+      
+      // reset the most significant bit
+      hashMask[hash_length - 1] = mask[ii];
+      hashSequence[hash_length - 1] = consensus[ii];
+      gapWindow[hash_length - 1] = gaps[ii];
+  }
+
+  multiset<Hash>::iterator h_itr;
+
+  // Remove duplicates
+  for( h_itr = h.begin() ; h_itr != h.end(); h_itr++ ){
+    if(h.count(*h_itr) == 1){
+        hs.insert(*h_itr);
+    }
+  }
+
+  return hs;
+}
 
 int main(int argc, char *argv[])
 {
--- hasher.h
+++ hasher.h
@@ -23,48 +23,96 @@ class Sequence;
 
 unsigned int HID = 0;
 class Hash {
-  public:
-    Hash();
-    Hash(boost::dynamic_bitset<>, boost::dynamic_bitset<>);
-    ~Hash(){};
-    bool match(boost::dynamic_bitset<>);
-    bool match(boost::dynamic_bitset<>, boost::dynamic_bitset<>);
-    bool operator<(const Hash) const;
-    bool operator>(const Hash) const;
-    unsigned int id(){ return _id; }
-    unsigned int size(){ return _size; }
-    boost::dynamic_bitset<> getMask(){ return _mask; }
-  private:
-    unsigned int _id;
-    unsigned int _size;
-    boost::dynamic_bitset<> _mask;
-    boost::dynamic_bitset<> _value;
+    public:
+        Hash();
+        Hash(boost::dynamic_bitset<>, boost::dynamic_bitset<>);
+        ~Hash(){};
+        bool match(boost::dynamic_bitset<>);
+        bool match(boost::dynamic_bitset<>, boost::dynamic_bitset<>);
+        bool operator<(const Hash) const;
+        bool operator>(const Hash) const;
+        unsigned int id(){ return _id; }
+        unsigned int size(){ return _size; }
+        boost::dynamic_bitset<> getMask(){ return _mask; }
+    private:
+        unsigned int _id;
+        unsigned int _size;
+        boost::dynamic_bitset<> _mask;
+        boost::dynamic_bitset<> _value;
 };
 
 class HashSet {
     public:
-      HashSet();
-      bool insert(Hash);
-      void match(boost::dynamic_bitset<>, boost::dynamic_bitset<>, std::multimap<int, int>&, int);
-      std::multimap<int, int> matchAll(Sequence);
-      int size(){ return _size; }
+        HashSet();
+        bool insert(Hash);
+        void match(boost::dynamic_bitset<>, boost::dynamic_bitset<>, std::multimap<int, int>&, int);
+        std::multimap<int, int> matchAll(Sequence);
+        int size(){ return _size; }
     private:
-      std::set<Hash> _hashes;
-      unsigned int _size;
+        std::set<Hash> _hashes;
+        unsigned int _size;
 };
 
 class Sequence {
-  public:
-    Sequence(kseq_t*);
-    Sequence ungap();
-    boost::dynamic_bitset<> getEncoded(){ return _seqbits; }
-    boost::dynamic_bitset<> getMask(){ return _qualityMask & _gapMask; }
-  private:
-    std::string _name;
-    std::string _comment;
-    std::string _seq;
-    std::string _qual;
-    boost::dynamic_bitset<> _seqbits;
-    boost::dynamic_bitset<> _qualityMask;
-    boost::dynamic_bitset<> _gapMask;
+    public:
+        Sequence(kseq_t*);
+        Sequence ungap();
+        boost::dynamic_bitset<> getEncoded(){ return _seqbits; }
+        boost::dynamic_bitset<> getMask(){ return _gapMask; }
+        std::string getName(){ return _name; }
+    private:
+
+        inline bool isNucleotide(char n){
+            switch(n){
+                case 'A':
+                case 'a':
+                case 'T':
+                case 't':
+                case 'C':
+                case 'c':
+                case 'G':
+                case 'g': return true;
+                default: return false;
+            }
+        }
+
+        std::string _name;
+        std::string _comment;
+        std::string _seq;
+        std::string _qual;
+        boost::dynamic_bitset<> _seqbits;
+        boost::dynamic_bitset<> _qualityMask;
+        boost::dynamic_bitset<> _gapMask;
 };
+/*
+class Reference : public Sequence {
+    public:
+        Reference();
+    private:
+        std::map<int, int> _hashMap;
+};
+*/
+
+class MultipleSequenceAlign { 
+    public:
+        MultipleSequenceAlign(){};
+        ~MultipleSequenceAlign(){};
+        int insert(kseq_t* s);
+        bool remove(std::string);
+        std::vector<int> getMinimalEncoding();
+        boost::dynamic_bitset<> getMask();
+        boost::dynamic_bitset<> getGaps();
+        boost::dynamic_bitset<> getConsensus();
+        HashSet makeHashes();
+    private:
+        //Member functions
+        void reset();
+        //Member variables
+        std::vector<Sequence> _sequences;
+        boost::dynamic_bitset<> _consensus;
+        boost::dynamic_bitset<> _gaps;
+        boost::dynamic_bitset<> _mask;
+        int _length;
+};
+
+
